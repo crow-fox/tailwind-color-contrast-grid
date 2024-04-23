7865f5340bc7c2120c548e5dfed4736cf3632a82
crow-fox <surakamiya198@gmail.com>
2024-04-23 13:15:17 +0900

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Tailwind Color Contrast",
@@ -15,7 +13,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className="font-sans grid grid-rows-[auto_1fr_auto] min-h-svh text-zinc-800">
+        <header className=" p-4 border-b border-b-zinc-300">
+          <p>
+            <Link href="/">Tailwind Color Contrast</Link>
+          </p>
+        </header>
+        <main className=" py-8 px-4">{children}</main>
+        <footer className="px-4 py-2 border-t border-t-zinc-300">
+          <p className="grid justify-center">
+            <a href="https://github.com/crow-fox">
+              <small className="text-sm">&copy; crow-fox</small>
+            </a>
+          </p>
+        </footer>
+      </body>
     </html>
   );
 }
