23f06673ce8ee830b5f4488a82070615e2c77144
crow-fox <surakamiya198@gmail.com>
2024-04-24 17:31:54 +0900

    getTailwindColors

--- app/_features/color/tailwind.ts
+++ app/_features/color/tailwind.ts
@@ -1,7 +1,41 @@
 import resolveConfig from "tailwindcss/resolveConfig";
 import tailwindConfig from "@/tailwind.config";
+import { DefaultColors } from "tailwindcss/types/generated/colors";
 
 export function getTailwindThemeColors() {
   const { theme } = resolveConfig(tailwindConfig);
   return theme.colors;
+}
+
+export function getTailwindColors(colors: DefaultColors) {
+  return {
+    graded: {
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
+    single: {
+      white: colors.white,
+      black: colors.black,
+    },
+  } as const;
 }
