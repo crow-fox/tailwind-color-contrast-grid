4d1b701f70f7f2b41a19f8011d7ddaeeb37f3d4d
crow-fox <surakamiya198@gmail.com>
2024-04-25 21:36:48 +0900

    色合いを変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,14 +13,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className="grid min-h-svh grid-rows-[auto_1fr_auto] font-mono text-zinc-800">
-        <header className=" border-b border-b-zinc-300 p-4">
+      <body className="grid min-h-svh grid-rows-[auto_1fr_auto] bg-white font-mono text-black">
+        <header className=" border-b border-b-slate-200 p-4">
           <p>
             <Link href="/">Tailwind Color Contrast Grid</Link>
           </p>
         </header>
         <main className=" px-4 py-8">{children}</main>
-        <footer className="border-t border-t-zinc-300 px-4 py-2">
+        <footer className="border-t border-t-slate-200 px-4 py-2">
           <p className="grid justify-center">
             <a href="https://github.com/crow-fox">
               <small className="text-sm">&copy; crow-fox</small>
