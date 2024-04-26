302f995dad35a9a03b0a5556ea6e0485e88a34aa
crow-fox <surakamiya198@gmail.com>
2024-04-26 21:36:59 +0900

    title変更

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="grid  justify-center  gap-y-8">
+    <div className="grid justify-center gap-y-8">
       <div className=" grid gap-y-2">
-        <h1 className=" text-lg font-bold">TailwindCSSカラーパレット</h1>
+        <h1 className=" text-lg font-bold">Tailwind Color Contrast Grid</h1>
         <Suspense>
           <ColorController />
         </Suspense>
