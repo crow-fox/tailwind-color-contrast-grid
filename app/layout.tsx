61f5fce1ac75748edd40965407888db9f1d24d09
crow-fox <surakamiya198@gmail.com>
2024-04-26 21:36:28 +0900

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Tailwind Color Contrast Grid",
@@ -13,14 +12,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className="grid min-h-svh grid-rows-[auto_1fr_auto] bg-white font-mono text-black">
-        <header className=" border-b border-b-slate-200 p-4">
-          <p>
-            <Link href="/">Tailwind Color Contrast Grid</Link>
-          </p>
-        </header>
-        <main className=" px-4 py-8">{children}</main>
-        <footer className="border-t border-t-slate-200 px-4 py-2">
+      <body className="grid min-h-svh grid-rows-[1fr_auto] bg-white font-mono text-black">
+        <main className="px-4 py-8">{children}</main>
+        <footer className="p-4">
           <p className="grid justify-center">
             <a href="https://github.com/crow-fox">
               <small className="text-sm">&copy; crow-fox</small>
