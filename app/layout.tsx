92a0b6f6271184668a3bca5309312d3a5dee701b
crow-fox <surakamiya198@gmail.com>
2024-04-28 19:25:01 +0900

    ThemeSelectをGlobal Layoutに追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { ThemeSelect } from "@/app/_features/darkmode/ThemeSelect";
 
 export const metadata: Metadata = {
   title: "Tailwind Color Contrast Grid",
@@ -15,7 +16,10 @@ export default function RootLayout({
     <html lang="ja" suppressHydrationWarning>
       <body>
         <ThemeProvider>
-          <div className="grid min-h-svh grid-rows-[1fr_auto] bg-white font-mono text-gray-900 dark:bg-gray-950 dark:text-gray-200">
+          <div className="grid min-h-svh grid-rows-[auto_1fr_auto] bg-white font-mono text-gray-900 dark:bg-gray-950 dark:text-gray-200">
+            <header className=" grid justify-end px-4 pt-4">
+              <ThemeSelect />
+            </header>
             <main className="px-4 py-8">{children}</main>
             <footer className="p-4">
               <p className="grid justify-center">
