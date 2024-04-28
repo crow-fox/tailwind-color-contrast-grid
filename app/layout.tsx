9b4360dd6ef09be49e8bbc2275c62b7429b5321c
crow-fox <surakamiya198@gmail.com>
2024-04-28 16:55:33 +0900

    next-themesを適応

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "Tailwind Color Contrast Grid",
@@ -11,18 +12,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <body>
-        <div className="grid min-h-svh grid-rows-[1fr_auto] bg-white font-mono text-black">
-          <main className="px-4 py-8">{children}</main>
-          <footer className="p-4">
-            <p className="grid justify-center">
-              <a href="https://github.com/crow-fox">
-                <small className="text-sm">&copy; crow-fox</small>
-              </a>
-            </p>
-          </footer>
-        </div>
+        <ThemeProvider>
+          <div className="grid min-h-svh grid-rows-[1fr_auto] bg-white font-mono text-gray-900 dark:bg-gray-950 dark:text-gray-200">
+            <main className="px-4 py-8">{children}</main>
+            <footer className="p-4">
+              <p className="grid justify-center">
+                <a href="https://github.com/crow-fox">
+                  <small className="text-sm">&copy; crow-fox</small>
+                </a>
+              </p>
+            </footer>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
