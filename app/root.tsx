ff59a1258244d4a68185df81a85227a5dc2a77ec
crow-fox <surakamiya198@gmail.com>
2024-08-11 15:36:07 +0900

    lang ja

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import "./globals.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
