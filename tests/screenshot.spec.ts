21d94d224093cd298d671db1d50047e72bcb30ba
crow-fox <surakamiya198@gmail.com>
2024-08-12 15:06:35 +0900

    playwright vrt

---
+++ tests/screenshot.spec.ts
@@ -0,0 +1,27 @@
+import test, { expect } from "@playwright/test";
+
+test(`screenshot[/]`, async ({ page }) => {
+  await page.goto("/");
+
+  // ページの読み込みを待つ
+  await page.getByRole("heading", {
+    level: 1,
+  });
+
+  // スクリーンショットを撮る
+  await expect(page).toHaveScreenshot({ fullPage: true });
+
+  // ColorGridを一色クリックした後のスクリーンショットを撮る
+  await page.getByRole("button", { name: "#f8fafc" }).click();
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test(`screenshot[reference]`, async ({ page }) => {
+  await page.goto("reference");
+
+  await page.getByRole("heading", {
+    level: 1,
+  });
+
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
