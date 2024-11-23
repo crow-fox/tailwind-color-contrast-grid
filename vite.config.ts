99f9b4f2a2d727c044789bacf2e52975d1be2af9
crow-fox <surakamiya198@gmail.com>
2024-11-23 16:34:35 +0900

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,12 @@
-import { copyFileSync } from "node:fs";
-import { join } from "node:path";
-import { vitePlugin as remix } from "@remix-run/dev";
+import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/tailwind-color-contrast-grid/"
-      : "/",
-  plugins: [
-    remix({
-      ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-      basename:
-        process.env.NODE_ENV === "production"
-          ? "/tailwind-color-contrast-grid/"
-          : "/",
-      buildEnd(args) {
-        if (!args.viteConfig.isProduction) return;
-        const buildPath = args.viteConfig.build.outDir;
-        copyFileSync(
-          join(buildPath, "index.html"),
-          join(buildPath, "404.html"),
-        );
-      },
-    }),
-  ],
-});
+export const BASE_PATH = "/tailwind-color-contrast-grid/";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+export default defineConfig({
+  base: BASE_PATH,
+  plugins: [reactRouter()],
+  server: {
+    open: BASE_PATH,
+  },
+});
