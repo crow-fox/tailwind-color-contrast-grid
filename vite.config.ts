27b5f336cef01a95636f669cb10819040bffaa66
crow-fox <surakamiya198@gmail.com>
2024-11-28 09:47:42 +0900

    postcssの設定をvite.configで一元管理してpostcss.configを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
 export const BASE_PATH = "/tailwind-color-contrast-grid/";
@@ -8,5 +10,10 @@ export default defineConfig({
   plugins: [reactRouter()],
   server: {
     open: BASE_PATH,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
   },
 });
